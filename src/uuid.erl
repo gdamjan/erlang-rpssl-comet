74fac3716d7b932129465b6d93797da05128f88d
Дамјан Георгиевски <gdamjan@gmail.com>
2018-06-24 21:32:34 +0200

    update to rand instead of random

--- src/uuid.erl
+++ src/uuid.erl
@@ -1,7 +1,6 @@
 % from http://stackoverflow.com/questions/1657204/erlang-uuid-generator
 -module(uuid).
 -behaviour(gen_server).
--import(random).
 
 %% API
 -export([start_link/0, generate/0]).
@@ -20,7 +19,7 @@ generate() ->
     gen_server:call(?SERVER, generate).
 
 init([]) -> 
-    random:seed(now()),
+    rand:seed(exs1024s),
     {ok, []}.
 
 handle_call(generate, _From, State) ->
@@ -36,8 +35,8 @@ code_change(_OldVsn, State, _Extra) -> {ok, State}.
 
 
 v4() ->
-    v4(random:uniform(16#FFFFFFFFFFFF), random:uniform(16#FFF), 
-            random:uniform(16#FFFFFFFF), random:uniform(16#3FFFFFFF)).
+    v4(rand:uniform(16#FFFFFFFFFFFF), rand:uniform(16#FFF),
+       rand:uniform(16#FFFFFFFF), rand:uniform(16#3FFFFFFF)).
 v4(R1, R2, R3, R4) ->
     <<R1:48, 4:4, R2:12, 2:2, R3:32, R4: 30>>.
 to_string(U) ->
