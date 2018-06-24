b177c7d954903cb0b76d178cf34606fc6a903b6d
Дамјан Георгиевски <gdamjan@gmail.com>
2018-06-24 21:20:03 +0200

    update mochiweb / rebar3 style directories
    * update mochiweb to a version fixed for erlang 21.0 which removed tuple
    calls (and a compiler flag is needed).
    
    ps. only rebar3 is used/tested now, and updates have been made to use
    the _build/ directory it creates.

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #! /bin/sh
 DIR=$(readlink -f $(dirname $0))
-export ERL_LIBS="$DIR:$DIR/deps/mochiweb"
+export ERL_LIBS="$DIR/_build/default/lib/"
 cd $DIR
 
 exec erl +Bd +K true -noinput -sasl errlog_type error \
