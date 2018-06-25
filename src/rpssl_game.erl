ceee6eebaf67ed6966c4ae8c8cd45e5ab454083c
Дамјан Георгиевски <gdamjan@gmail.com>
2018-06-25 14:42:13 +0200

    use binaries more

--- src/rpssl_game.erl
+++ src/rpssl_game.erl
@@ -7,11 +7,11 @@ play(PlayerAttack) ->
     ComputerAttack = get_computer_attack(),
     case get_result(PlayerAttack, ComputerAttack) of
         {win,_} ->
-            [": I chose ", atom_to_list(ComputerAttack), ". You win!"];
+            <<": I chose ", ComputerAttack, ". You win!">>;
         {draw,_} ->
-            [": I chose ", atom_to_list(ComputerAttack), ". It's a draw."];
+            <<": I chose ", ComputerAttack, ". It's a draw.">>;
         {lose,_} ->
-            [": I chose ", atom_to_list(ComputerAttack), ". I WIN!"]
+            <<": I chose ", ComputerAttack, ". I WIN!">>
     end.
 
 play(Player1, Player2) ->
@@ -20,23 +20,23 @@ play(Player1, Player2) ->
 %% choose a computer attack at random
 get_computer_attack() ->
     %% Get an index position at random
-    Index = random:uniform(5),
+    Index = rand:uniform(5),
     %% Pull out an attack
-    lists:nth(Index, ["rock", "paper", "scissors", "spock", "lizard"]).
+    lists:nth(Index, [<<"rock">>, <<"paper">>, <<"scissors">>, <<"spock">>, <<"lizard">>]).
 
 %% Determine the result of an attack
 get_result(Player1, Player2) ->
     case {Player1, Player2} of
-        {"rock", "scissors"} -> {win, lose};
-        {"rock", "lizard"} -> {win, lose};
-        {"paper", "rock"} -> {win, lose};
-        {"paper", "spock"} -> {win, lose};
-        {"scissors", "paper"} -> {win, lose};
-        {"scissors", "lizard"} -> {win, lose};
-        {"spock", "rock"} -> {win, lose};
-        {"spock", "scissors"} -> {win, lose};
-        {"lizard", "spock"} -> {win, lose};
-        {"lizard", "paper"} -> {win, lose};
+        {<<"Rock">>, <<"Scissors">>} -> {win, lose};
+        {<<"Rock">>, <<"Lizard">>} -> {win, lose};
+        {<<"Paper">>, <<"Rock">>} -> {win, lose};
+        {<<"Paper">>, <<"Spock">>} -> {win, lose};
+        {<<"Scissors">>, <<"Paper">>} -> {win, lose};
+        {<<"Scissors">>, <<"Lizard">>} -> {win, lose};
+        {<<"Spock">>, <<"Rock">>} -> {win, lose};
+        {<<"Spock">>, <<"Scissors">>} -> {win, lose};
+        {<<"Lizard">>, <<"Spock">>} -> {win, lose};
+        {<<"Lizard">>, <<"Paper">>} -> {win, lose};
         {Same, Same} -> {draw, draw};
         {_,_} -> {lose, win}
     end.
